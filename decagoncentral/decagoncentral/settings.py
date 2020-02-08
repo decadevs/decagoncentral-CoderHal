fd8d271761139d05f7f18f175bb634d3e6cb71a3
CoderHal <halof.halima@gmail.com>
2020-02-08 18:01:10 +0100

    create user model

--- decagoncentral/decagoncentral/settings.py
+++ decagoncentral/decagoncentral/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,12 @@ WSGI_APPLICATION = 'decagoncentral.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'userdb',
+        'USER': 'postgres',
+        'PASSWORD': 'curly',
+        'HOST': 'localhost',
     }
 }
 
