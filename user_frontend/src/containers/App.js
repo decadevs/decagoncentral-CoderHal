cfd1d3f06e1136d17829948b0e44f2bcfaea31ac
CoderHal <halof.halima@gmail.com>
2020-02-11 15:23:32 +0100

    resolve conflict

--- user_frontend/src/containers/App.js
+++ user_frontend/src/containers/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
 import '../App.css';
-import MainContainer from './MainContainer';
-import Sidebar from './Sidebar'
+// import MainContainer from './MainContainer';
+// import Sidebar from './Sidebar'
+// import LoginBg from '../components/Login_bg'
+// import Login from '../containers/Login'
+import Signup from './Signup';
+// import Login from '..components/Login'
 
 function App() {
   return (
-    <div className="App row">
-      <Sidebar/>
-      <MainContainer/>
+    <div className="container">
+      {/* <Login/> */}
+      {/* <Sidebar/>
+      <MainContainer/> */}
+      <Signup/>
     </div>
   );
 }
 
+export default App;
